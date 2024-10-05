968ee7704255a7a1b5196cc3088cee28ebe35f1f
bellflwr <70229194+bellflwr@users.noreply.github.com>
2024-10-05 11:54:37 -0400

    Made main page and added modules

--- exo25_web/app.py
+++ exo25_web/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
 def create_app() -> Flask:
+    from .blueprints import home
+
     app = Flask(__name__)
 
-    @app.route("/")
-    def home():
-        return "Hello exoworld"
+    app.register_blueprint(home, url_prefix="/")
     
     return app

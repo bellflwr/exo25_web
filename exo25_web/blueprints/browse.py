28c1855d4338d7244f64650d1753670c07c4be85
Andrei <142443853+hjzres@users.noreply.github.com>
2024-10-05 15:24:09 -0400

    made browse look good

--- exo25_web/blueprints/browse.py
+++ exo25_web/blueprints/browse.py
@@ -1,3 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 
 browse = Blueprint("browse", __name__)
+
+@browse.route("/")
+def index():
+    search = "test"
+    return redirect(url_for("browse.search", search=search))
+
+@browse.route("/<string:search>", methods=["GET", "POST"])
+def search(search: str):
+    return render_template("browse.html")

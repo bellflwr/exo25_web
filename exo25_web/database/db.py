da35495451540c87d1251c8731ca31214b7d76d4
W1ckle <oprutac@gmail.com>
2024-10-06 13:59:20 -0400

    Added error handling for when no row is found in from_id() and from_name() Added explanatory comments

--- exo25_web/database/db.py
+++ exo25_web/database/db.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 import sqlite3
 from flask import g
 
-con = sqlite3.connect("Exoplanets.db")
+con = sqlite3.connect("../../Exoplanets.db")
 # cur = con.cursor()
 
 DATABASE = "Exoplanets.db"
@@ -17,6 +17,7 @@ def get_db():
 
 @dataclass
 class Exoplanet:
+    # Instantiating all attributes
     name: str
     planet_type: str
     diameter: int
@@ -28,25 +29,37 @@ class Exoplanet:
     climate: int
     star_type: str
 
+    # Takes a cursor and id of a row/exoplanet
     @classmethod
     def from_id(cls, cur, exoplanet_id):
+        # Takes exoplanet_id and finds row with corresponding ID
         exoplanet_query = cur.execute(
             "SELECT Name, Type, Diameter, Distance, Material, Gas, Rings, Colour,"
             "Climate, StarType FROM Exoplanets WHERE ID = ?",
             (exoplanet_id,),
         ).fetchone()
+        # Checks if no row was found
+        if exoplanet_query is None:
+            return None
         return cls(*exoplanet_query)
 
+    # Takes a cursor and name of an exoplanet
     @classmethod
     def from_name(cls, cur, exoplanet_name):
+        # Takes exoplanet_id and finds row with corresponding name
         exoplanet_query = cur.execute(
             "SELECT Name, Type, Diameter, Distance, Material, Gas, Rings, Colour,"
             "Climate, StarType FROM Exoplanets WHERE Name = ?",
             (exoplanet_name,),
         ).fetchone()
+        # Checks if no row was found
+        if exoplanet_query is None:
+            return None
         return cls(*exoplanet_query)
 
+    # Inserts exoplanet object into database
     def commit(self, cur):
+        # Makes tuple of all data to insert into SQL statement
         inserted_tuple = (
             self.name,
             self.planet_type,
@@ -59,6 +72,7 @@ class Exoplanet:
             self.climate,
             self.star_type,
         )
+        # Inserts object data into database and commits it
         cur.execute(
             "INSERT INTO Exoplanets(Name, Type, Diameter, Distance, Material, Gas, Rings, Colour, Climate,"
             "StarType) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
@@ -66,6 +80,7 @@ class Exoplanet:
         )
         con.commit()
 
+    # Counts total amount of planet rows in table
     @staticmethod
     def exoplanet_count(cur):
         return cur.execute("SELECT COUNT(*) FROM Exoplanets").fetchone()[0]
@@ -76,3 +91,6 @@ class Exoplanet:
 # planet.commit()
 
 # print(cur.execute("SELECT * FROM Exoplanets").fetchone())
+
+# planet2 = Exoplanet.from_id(cur, 1)
+# print(planet2)
